85c87406402b1a3048b18b5e31580edc9941140a
kyattyan <k.yattyan349245@gmail.com>
2018-11-18 17:28:09 +0900

    Key finder and key editer
    Addition of FindNextNum() and SetNextNum(), to find a keyword for the next LocalFigure number, and edit it.
    However, they does not work well in my functions, especialy, handleFileSelect().

--- Uploader.js
+++ Uploader.js
@@ -78,6 +78,8 @@ $(function() {
 //-----------------------------ここより、ローカルファイル用---------------------------------------------------------
 function handleFileSelect(evt) {
     
+    
+
     evt.stopPropagation();
     evt.preventDefault();
 
@@ -98,8 +100,9 @@ function handleFileSelect(evt) {
         return function(e) {
           // Render thumbnail.
             var span = document.createElement('span');
-          
-            span.innerHTML = ['Figure'+ escape(String(Int+1)) + ': <img class="thumb" src="', e.target.result,
+            
+            //'Figure'+ escape(String(Int+1)) + 
+            span.innerHTML = ['<img class="thumb" align = "left" src="', e.target.result,
                             '" title="', escape(theFile.name), '", id="LocalFig',Int+1,'"/>',
                             //表示位置用コンボボックス
                             '表示位置/Float: <select name="Float_LocalFig' + escape(String(Int+1)) + '" size = "1" ',
@@ -113,11 +116,14 @@ function handleFileSelect(evt) {
                             ].join('');
             document.getElementById('ThumbList').insertBefore(span, null);
         };
+        
       })(f, i);
 
       // Read in the image file as a data URL.
       reader.readAsDataURL(f);
+      
     }
+    
   }
 
 function handleDragOver(evt) {
@@ -127,4 +133,72 @@ function handleDragOver(evt) {
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
 }
 
+function FindNextNum(){
+    SetNextNum();
+    //サムネイルリスト部分のHTMLの生のコード取得
+    var S_LowHTML = document.getElementById('ThumbList');
+    //var S_FullComment_Next = String(S_LowHTML).match(/<!--Next:\d{1,}-->/);
+    var S_FullComment_Next = String(S_LowHTML.outerHTML).match(/<!--Next:\d{1,}-->/);
+    //alert(S_FullComment_Next);
+    var S_Next = String(S_FullComment_Next).slice(9,-3);
+    return parseInt(S_Next,10);
+}
+
+function SetNextNum(){
+
+    var S_LowHTML = document.getElementById('ThumbList').outerHTML;
+
+    //一番最初のLocalFigureを検索
+    var S_LocalFigs = S_LowHTML.match(/id="LocalFig\d{1,}"/g);
+    
+    var I_NextNum = 0;
+
+    if(S_LocalFigs==null){
+        I_NextNum = 1;
+    }else if(S_LocalFigs.length == 1){
+        //Figure1か否かで切り替え
+        if(S_LocalFigs[0]=='id="LocalFig1"'){
+            I_NextNum = 2;
+        }else{
+            I_NextNum = 1;
+        }
+    }else{
+        //要素を二つずつ比較
+        for(var i = 0; i+1 < S_LocalFigs.length; i++){
+            //配列から数字だけ抽出。配列の若い番号に大きいFigure番号が格納されているので、後ろから順々に比較
+            var S_PreviousNum = S_LocalFigs[S_LocalFigs.length-i-1].slice(12, -1);
+            var S_LaterNum = S_LocalFigs[S_LocalFigs.length-i-2].slice(12, -1);
+
+            //キャスト
+            var I_PreviousNum = parseInt(S_PreviousNum, 10);
+            var I_LaterNum = parseInt(S_LaterNum, 10);
+            //alert('Pre: ' + I_PreviousNum + ', Later: ' + I_LaterNum);
+            //数字の差が1より大きいとき、小さい方+1の数字をNextに設定
+            if(I_LaterNum - I_PreviousNum > 1){
+                I_NextNum = I_PreviousNum + 1;
+            
+            //最後までは走り切った場合、大きい方+1をNextに設定
+            }else if(i+2 == S_LocalFigs.length){
+                I_NextNum = I_LaterNum + 1;
+            }
   
+        }
+        
+    }
+
+    //var S_LowHTML = $('#ThumList').val();
+
+    var S_SetHTML = S_LowHTML.replace(/<!--Next:\d{1,}-->/, '<!--Next:'+I_NextNum+'-->');
+    alert(S_SetHTML);
+    
+    document.getElementById('ThumbList').innerHTML = S_SetHTML;
+
+    return;
+   
+}
+
+
+//各関数動作確認用
+function test(){
+    alert(FindNextNum());
+}
