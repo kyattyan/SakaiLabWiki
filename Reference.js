e39bf324de71aa2c89ca9b0b228376c124048cd6
kyattyan <k.yattyan349245@gmail.com>
2018-11-29 17:55:14 +0900

    Textboxes for reference
    Textboxes to input information for referred book or URL was set up.
    All of them does not work yet.

--- Reference.js
+++ Reference.js
@@ -16,7 +16,8 @@ function NewReference(){
     switch(S_ReferenceType){
         case '論文/Journal':
             S_AddedHTML+='<span id="Refrence0">Refernce 0　　'+
-                'Authors: <input type="text" Name="Authors" style="width: 60%;"><br>'+
+                'Authors: <input type="text" Name="Authors" style="width: 50%;"> '+
+                'doi: <input type="text" Name="doi" style="width: 25%;"><br>'+
                 'Journal: <input type="text" Name="J_Name" > '+
                 'Vol: <input type="number" Name="J_Vol" value="1" width = "80" min = "1" max = "200"> '+
                 'Published year: <input type="number" Name="J_Year" value="2000" min="1800" max="2100"> '+
@@ -25,10 +26,19 @@ function NewReference(){
                 '</span><br>';
             break;
         case  '本/Book':
-            
+            S_AddedHTML+='<span id="Refrence0">Refernce 0　　'+
+                'Title: <input type="text" Name="B_Title"  style="width: 50%;"> '+
+                'Author: <input type="text" Name="Authors"  style="width: 25%;"> '+
+                'Publisher: <input type="text" Name="B_Publisher" > '+
+                '</span><br>';
             break;
         case 'URL':
-            
+            S_AddedHTML+='<span id="Refrence0">Refernce 0　　'+
+                'URL: <input type="text" Name="U_URL"  style="width: 70%;"><br>'+
+                '閲覧日/Date you saw: Y<input type="number" Name="U_SeenDate_Y" value = "2018" min="2018" max="3000"> '+
+                'M<input type="number" Name="U_SeenDate_M" value="1" min="1" max="12"> '+
+                'D<input type="number" Name="U_SeenDate_D" value="1" min="1" max="31"> '+
+                '</span><br>';
             break;
         case 'その他/The other':
             
